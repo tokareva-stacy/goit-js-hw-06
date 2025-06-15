3c1ff25644c0c6458aa1e1b1cd9156e3a159088e
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-15 10:05:12 +0300

    task 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,12 @@
-function filterArray(numbers, value) {
-    const result = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            result.push(numbers[i]); 
-        }
-      }
-    
-      return result;
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
