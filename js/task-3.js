705bc65c01278ff29a64bbd467333c7c4998d33a
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-19 17:41:50 +0300

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,22 @@
-
-
-
+class StringBuilder {
+    #value;
+    
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value += str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+}
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
