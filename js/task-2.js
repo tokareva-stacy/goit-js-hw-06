f9fce3f28e5c428961e2fbeec424cf6b7085f343
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-19 16:55:03 +0300

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,17 @@
-
-
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+    getItems() {
+        return this.items;
+    }
+    addItem(newItem) {
+        this.items.push(newItem);
+    }
+    removeItem(itemToRemove) { 
+        this.items = this.items.filter(item => item !== itemToRemove);
+    }
+}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
