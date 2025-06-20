defe85bc59a0fb0ae914e9a21ab2bd57a094a312
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-20 21:52:31 +0300

    change items - #items

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,17 @@
 class Storage {
+    #items;
+
     constructor(items) {
-        this.items = items;
+        this.#items = items;
     }
     getItems() {
-        return this.items;
+        return this.#items;
     }
     addItem(newItem) {
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
     removeItem(itemToRemove) { 
-        this.items = this.items.filter(item => item !== itemToRemove);
+        this.#items = this.#items.filter(item => item !== itemToRemove);
     }
 }
 
